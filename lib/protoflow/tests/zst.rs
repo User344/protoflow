ff99fc74116cdbd44da1c7bbc1b871cb5dec318a
Yura Menshov <user344u@gmail.com>
2024-11-25 13:28:10 +0200

    add build error in a test

--- lib/protoflow/tests/zst.rs
+++ lib/protoflow/tests/zst.rs
@@ -21,6 +21,8 @@ fn const_with_numeric_zero() -> Result<(), ()> {
         process.join().unwrap();
     });
 
+    runtime.cook();
+
     assert_eq!(output.recv(), Ok(Some(42))); // not Ok(None)
     Ok(())
 }
